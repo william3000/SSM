da9f6950ffcf14d17441944135f6f5604845ea61
zhangduping <zhangduping@wondersgroup.com>
2018-03-27 15:17:11 +0800

    redis-session

---
+++ ChInterface/src/main/java/com/cpic/chinterface/controller/TestSessionController.java
@@ -0,0 +1,33 @@
+package com.cpic.chinterface.controller;
+
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by admin on 2017/4/16.
+ */
+@Controller
+public class TestSessionController {
+
+	@RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+	
+	@RequestMapping("/put")
+    public String put(HttpSession session) {
+        session.setAttribute("name", "simon");
+        return "index";
+    }
+	
+	@RequestMapping("/get")
+	@ResponseBody
+    public String get(HttpSession session) {
+		return (String) session.getAttribute("name");
+    }
+}
